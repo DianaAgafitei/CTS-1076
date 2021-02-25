6c8e26f4e644925e5f9a7ee8728f9189898c1ddb
DianaAgafitei <agafiteimarina18@stud.ase.ro>
2021-02-25 13:30:43 +0200

    clase modificate

--- Seminar1/src/pachet/Main.java
+++ Seminar1/src/pachet/Main.java
@@ -1,6 +1,7 @@
 package pachet;
 
 import clase.Giraffe;
+import clase.Zebra;
 import clase.Zoo;
 
 public class Main {
@@ -9,6 +10,9 @@ public class Main {
 		Zoo zoo=new Zoo();
 		Giraffe g1= new Giraffe("Maia",300);
 		Giraffe g2 = new Giraffe("Lola");
+		
+		Zebra z1=new Zebra("Thom",2);
+		zoo.addAnimal(z1);
 		
 		zoo.addAnimal(g1);
 		zoo.addAnimal(g2);
