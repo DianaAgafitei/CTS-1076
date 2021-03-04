f81b952ba6d1f7ea890ea646bbcf3a6b2a9cbf27
DianaAgafitei <agafiteimarina18@stud.ase.ro>
2021-03-04 13:19:33 +0200

    Final commit

--- Seminar2/src/ro/ase/cts/clase/UniversalReader.java
+++ Seminar2/src/ro/ase/cts/clase/UniversalReader.java
@@ -3,6 +3,8 @@ package ro.ase.cts.clase;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
+import ro.ase.cts.clase.readers.Reader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,5 +13,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UniversalReader {
-//TO DO punct de legatura cu readerele noastre
+ 
+	public static List<Aplicant> readAplicants(Reader reader) throws NumberFormatException, FileNotFoundException{
+		return reader.readAplicants();
+	}
 }
